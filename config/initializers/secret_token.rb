d0a2e437a2cdcdd209d460e0cfb3db174e7b9fb1
Larry Scott <iamjustfresh@gmail.com>
2011-10-11 07:49:42 -0400

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtasystem::Application.config.secret_token = '76efea0b510cc9ffe0bca02391e829be7cd7fbfda0be33869ce399a4ab1052926220048db75b3f7d23cfb71d9f9e2974409b9d86c5eee57bf05b6de8619ee978'
