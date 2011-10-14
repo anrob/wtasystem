421b7bfa35f6709de5a39075649c835967614d3b
Larry Scott <iamjustfresh@gmail.com>
2011-10-14 17:25:22 -0400

    shaina

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+//= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery-1.4.2.min.js
-//= require highcharts.js
+//= require_tree .
