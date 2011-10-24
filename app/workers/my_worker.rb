23adf24229d96d55e668e17d77a2f6d3b5472e96
Larry Scott <iamjustfresh@gmail.com>
2011-10-24 12:49:29 -0400

    working with our worker file

--- app/workers/my_worker.rb
+++ app/workers/my_worker.rb
@@ -3,12 +3,12 @@ class MyWorker < SimpleWorker::Base
   def run
        require 'csv'
        require 'net/ftp'
-      #Dir.chdir(Rails.root.join("tmp")) do
+      Dir.chdir("tmp") do
                Net::FTP.open("ftp.dctalentphotovideo.com") do |ftp|
                  ftp.passive = true
                  ftp.login('telemagic@dctalentphotovideo.com', 'shaina99')
-                 ftp.chdir("uploads")
-                 file = ftp.nlst("*.txt")
+                 #ftp.chdir("uploads")
+                 file = ftp.nlst("*.TXT")
                  #@file = file
                  file.each{|filename| #Loop through each element of the array
                    ftp.getbinaryfile(filename,filename) #Get the file
