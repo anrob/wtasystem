8a56717c6de7e77fa19c501f64652618fc43a2fa
Larry Scott <iamjustfresh@gmail.com>
2011-10-26 23:11:51 -0400

    remove

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ resources :users
    match '/otheracts', :to => "contracts#otheracts"
    match '/import', :to => "contracts#import_contracts"
    match '/alljobs', :to => "contracts#alljobs"
-   match '/mailchimp', :to => "contracts#mailchimp"
-   match '/calendar', :to => "contracts#calendar" 
    #match '/otheracts/*actcode', :to => "contracts#otheracts"
    match '/users/:id'  => "users#edit"
 end
