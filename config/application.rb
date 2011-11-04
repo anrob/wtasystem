2cb909d68a51f7dd35c09f55d2f83355c483044d
Larry Scott <iamjustfresh@gmail.com>
2011-11-04 16:34:00 -0400

    assets precomplie

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Wtasystem
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-     config.i18n.default_locale = :en
+    # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -41,9 +41,8 @@ module Wtasystem
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.debug = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.7'
+    config.assets.version = '1.8'
   end
 end
