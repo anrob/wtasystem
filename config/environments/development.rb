cfca181252aee941f9f4f906257b7d405cbcf4e7
Larry Scott <iamjustfresh@gmail.com>
2011-11-17 13:40:35 -0500

    remove debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,13 +18,13 @@ Wtasystem::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
   config.assets.debug = false
