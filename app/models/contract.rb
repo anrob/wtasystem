9de76f6a3157b06e6312fe8302c08438adcdb09b
Larry Scott <iamjustfresh@gmail.com>
2011-11-23 17:57:59 -0500

    sms

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -75,7 +75,8 @@ class Contract < ActiveRecord::Base
          @recipients = @users.collect {|m| m.email}
          @recipients_number = @users.collect {|m| m.phone_number}
          #ContractMailer.send_reminder(@recipients).deliver
-         Moonshado::Sms.new("#{@recipients_number}", "Your Have Events to confirm. Please log-in").deliver_sms
+         sms = Moonshado::Sms.new("#{@recipients_number}", "Your Have Events to confirm. Please log-in")
+         sms.deliver_sms
      end
      
 def self.mailchimp
