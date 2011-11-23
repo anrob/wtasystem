d57c5fee6e23aa32d408941c94afc154ad3e53ab
Larry Scott <iamjustfresh@gmail.com>
2011-11-22 22:45:46 -0500

    id

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 AssetSync.configure do |config|
-  config.aws_access_key_id_id = 'AKIAJIZUFUFDNZP67G3Q'
+  config.aws_access_key_id = 'AKIAJIZUFUFDNZP67G3Q'
   config.aws_secret_access_key = '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'
   config.fog_directory = 'wtasystem-assets'
 end
