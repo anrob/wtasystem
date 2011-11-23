04c261f81a389523d72b8d75cc521b32f5b82d30
Larry Scott <iamjustfresh@gmail.com>
2011-11-22 22:40:00 -0500

    init

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,5 @@
-AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['AWS_KEY'],
-  :secret_access_key => ENV['AWS_SECRET']
-)
+AssetSync.configure do |config|
+  config.aws_access_key_id_id = 'AKIAJIZUFUFDNZP67G3Q'
+  config.aws_secret_access_key = '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'
+  config.fog_directory = 'wtasystem-assets'
+end
