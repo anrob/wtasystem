71283a3331d1988330b7eb322260a71a35b6236e
Larry Scott <iamjustfresh@gmail.com>
2011-12-27 13:25:43 -0500

    colored confirmed

--- app/models/actcode.rb
+++ app/models/actcode.rb
@@ -1,7 +1,7 @@
 class Actcode < ActiveRecord::Base
   belongs_to :management
  has_many :users
- attr_accessible :actcode, :description
+ attr_accessible :actcode, :description, :management_id
  
   default_scope :order => 'actcode ASC'
   scope :getallbycompany, lambda { |acts| where("management_id = ?", acts.management_id) }
