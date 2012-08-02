55e14a3164b89a50125c241c41d63073724e3d3e
Larry Scott <iamjustfresh@gmail.com>
2012-08-02 14:42:21 -0400

    skip

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,5 +3,5 @@ class BaseController < ApplicationController
   inherit_resources
   load_and_authorize_resource
  skip_before_filter :authenticate_user!, :except => [:new, :create]
-  skip_before_filter :everypage
+ skip_before_filter :everypage
  end
