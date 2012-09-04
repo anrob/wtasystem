79a985a4f1f620f1e06aa535ec778bddd1f90c8c
Larry Scott <iamjustfresh@gmail.com>
2012-09-04 18:19:31 -0400

    remove migration

--- db/migrate/20120904160333_add_index_to_contracts.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToContracts < ActiveRecord::Migration
-  def self.up
-      add_index :contracts, :unique3, :unique => true
-    end
-
-    def self.down
-      remove_index :contracts, :column => :unique3
-    end
-end
