867e464cc6a53cd88ad8f687c7e2383438653516
Larry Scott <iamjustfresh@gmail.com>
2013-08-09 10:39:37 -0400

    Reset so job fail if no good

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Wtasystem::Application.routes.draw do
   match "Working/500", :to => "working#error"
 
   match "Working/404", :to => "working#not_found"
-
+  root to: "contracts#index"
   ActiveAdmin.routes(self)
 
-resources :actnotes, :managements, :contracts, :messages, :actcodes, :incoming_mails,:quotes,:messages
+resources :actnotes, :managements, :contracts, :messages, :actcodes, :incoming_mails,:quotes,:messages, :users
 devise_for :users, controllers: {sessions: 'devise/sessions',:confirmations => "confirmations"}, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'cmon_let_me_in' },skip: [:sessions] do
                  get 'signin'  => 'devise/sessions#new', as: :new_user_session
                  post 'signin' => 'devise/sessions#create', as: :user_session
@@ -20,9 +20,6 @@ devise_for :users, controllers: {sessions: 'devise/sessions',:confirmations => "
                    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
 
                end
-
-resources :users
-   root to: "contracts#index"
    match '/confirmjob', to: "contracts#confirmjob"
    match '/emailjobwithnetonly', to: "contracts#emailjobwithnetonly"
    match '/emailjobwithallmoney', to: "contracts#emailjobwithallmoney"
@@ -42,4 +39,5 @@ resources :users
    match '/incoming', to: "incoming_mails#create"
    #map.connect ':controller/:action/:id'
    match '/report', to: "contracts#report"
+
 end
